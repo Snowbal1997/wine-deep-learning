84828e00da80dd1a970e8059996ccac0e5923d2b
Elianne <elianne.heuer@hotmail.com>
2023-06-30 15:11:13 +0200

    Cleaned up version

--- scrape-winemag.py
+++ scrape-winemag.py
@@ -1,15 +1,6 @@
-import argparse
-
-from bs4 import BeautifulSoup as soup
-from multiprocessing.dummy import Pool
-import os
-import shutil
-import time
 import requests
-import re
 import json
-import glob
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 import pandas as pd
 
 
@@ -27,7 +18,7 @@ if __name__ == "__main__":
         "country",
     ]
     wine_df = pd.DataFrame(columns=wine_cols)
-    # min_price = str(0)  # 0 red
+    min_price = str(0)  # 0 red
     # min_price = str(15.41)  # 1 red
     # min_price = str(25.21)  # 2 red
     # min_price = str(41.26)  # 3 red
@@ -43,7 +34,8 @@ if __name__ == "__main__":
 
     # try_name = "1"
 
-    for x in range(0, 85):
+    # for x in range(0, 85):
+    for x in range(0, 2):
         r = requests.get(
             "https://www.vivino.com/api/explore/explore",
             params={
@@ -97,13 +89,13 @@ if __name__ == "__main__":
 
         wine_dict = json.loads(json.dumps(r.json()))
 
-        with open("white1_" + str(x) + ".json", "w") as fp:
+        with open("testtt" + str(x) + ".json", "w") as fp:
             json.dump(wine_dict, fp, sort_keys=True, indent=4)
 
     print(wine_df.shape)
     print(wine_df.head)
 
-    wine_df.to_csv("white1.csv")
+    wine_df.to_csv("testtt.csv")
     print(min_price)
 
     # https://stackoverflow.com/questions/71264253/web-scraping-vivino-using-python
